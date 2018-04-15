34749df4bfe03587c53d10d5e9ace9a293b57143
Alex Rocha <alex.rochas@yahoo.com.br>
2018-04-15 17:18:55 -0300

    green: add pet

--- src/main/java/br/com/alex/camel/kt/api/PetApi.java
+++ src/main/java/br/com/alex/camel/kt/api/PetApi.java
@@ -4,6 +4,7 @@ import br.com.alex.camel.kt.repository.PetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,6 +16,11 @@ public class PetApi {
   @GetMapping("/pets")
   public ResponseEntity<?> fetchPets() {
     return ResponseEntity.status(200).body(petRepository.findAll());
+  }
+
+  @PostMapping
+  public ResponseEntity<?> addPet() {
+    return ResponseEntity.ok(200);
   }
 
 }
