9c004a9ed3e4237d7c46b6b6e608efc5094c272c
Alex Rocha <alex.rochas@yahoo.com.br>
2018-04-15 18:20:45 -0300

    green: search for one pet

--- src/main/java/br/com/alex/camel/kt/api/PetApi.java
+++ src/main/java/br/com/alex/camel/kt/api/PetApi.java
@@ -3,11 +3,14 @@ package br.com.alex.camel.kt.api;
 import br.com.alex.camel.kt.dto.PetDto;
 import br.com.alex.camel.kt.entity.PetEntity;
 import br.com.alex.camel.kt.repository.PetRepository;
+import java.util.Map;
+import jersey.repackaged.com.google.common.collect.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,7 +20,15 @@ public class PetApi {
   private PetRepository petRepository;
 
   @GetMapping("/pets")
-  public ResponseEntity<?> fetchPets() {
+  public ResponseEntity<?> fetchPets(@RequestParam Map<String, String> queryParams) {
+    String name = queryParams.get("name");
+    if (name != null && !name.isEmpty()) {
+      for (PetEntity petEntity : petRepository.findAll()) {
+        if (petEntity.getName().equals(name)) {
+          return ResponseEntity.ok(Lists.newArrayList(convertEntityToDto(petEntity)));
+        }
+      }
+    }
     return ResponseEntity.status(200).body(petRepository.findAll());
   }
 
--- src/test/java/br/com/alex/camel/kt/cucumber/InsertPetStepdefs.java
+++ src/test/java/br/com/alex/camel/kt/cucumber/InsertPetStepdefs.java
@@ -1,10 +1,13 @@
 package br.com.alex.camel.kt.cucumber;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import br.com.alex.camel.kt.dto.PetDto;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import cucumber.api.java8.En;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 
@@ -18,10 +21,17 @@ public class InsertPetStepdefs implements En {
       world.map.put("request", new PetDto(name));
     });
 
-    And("^return Pet with name (.*)$", (name) -> {
+    And("^return Pet with name (.*)$", (String name) -> {
       ObjectMapper objectMapper = new ObjectMapper();
-      assertEquals(objectMapper.convertValue(((ResponseEntity<PetDto>) world.map.get("response")).getBody(),
-              PetDto.class).getName(), name);
+      ResponseEntity<PetDto> response = ((ResponseEntity<PetDto>) world.map.get("response"));
+      if (response.getBody() instanceof List) {
+        List<PetDto> pets = objectMapper.convertValue(response.getBody(),
+                new TypeReference<List<PetDto>>() {
+                });
+        assertTrue(pets.stream().anyMatch(p -> p.getName().equals(name)));
+      } else {
+        assertEquals(objectMapper.convertValue(response.getBody(), PetDto.class).getName(), name);
+      }
     });
   }
 }
